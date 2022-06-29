78a10b65b91f49282c1543ebc70212eefc97e079
mongmong0515 <106512154+mongmong0515@users.noreply.github.com>
2022-06-29 18:38:09 +0900

    220629_sk_siltron_chapter.01

---
+++ assets/playbar/playbarScript.js
@@ -0,0 +1,50 @@
+cp.playbarAssetArr = 
+[
+	'AudioOff',
+	'AudioOn',
+	'BackGround',
+	'Backward',
+	'Color',
+	'ColorSmall',
+	'CC',
+	'Exit',
+	'FastForward',
+	'FastForward1',
+	'FastForward2',
+	'Forward',
+	'Glow',
+	'GlowSmall',
+	'Height',
+	'InnerShade',
+	'InnerShadeSmall',
+	'Play',
+	'Pause',
+	'Progress',
+	'Rewind',
+	'Stroke',
+	'StrokeSmall',
+	'Thumb',
+	'ThumbBase',
+	'TOC'
+];
+cp.playbarTooltips = 
+{
+	AudioOff : "오디오 켜기 ", 
+	AudioOn : "오디오 끄기 ", 
+	Backward : "뒤로 ", 
+	CC : "폐쇄 캡션 ", 
+	Exit : "끝내기 ", 
+	FastForward : "2배속 빨리 감기 ", 
+	FastForward1 : "4배속 빨리 감기 ", 
+	FastForward2 : "일반 속도 ", 
+	Forward : "앞으로 ", 
+	Play : "재생 ", 
+	Pause : "일시 중지 ", 
+	Rewind : "되감기 ", 
+	TOC : "목차 ", 
+	Info : "정보 ", 
+	Print : "인쇄 "
+};
+cp.handleSpecialForPlaybar = function(playbarConstruct)
+{
+}
